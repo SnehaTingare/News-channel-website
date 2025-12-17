9c23d1584d3dde0dda3db3a5ca8c2c23c321491e
SnehaTingare <snehatingare42@gmail.com>
2025-12-17 12:05:53 +0530

    added mongodb atlas database

--- channel-website/backend/index.js
+++ channel-website/backend/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+mongoose.connect(
+  "mongodb+srv://snehatingare42_db_user:SUN1ight@cluster0.eoawpkz.mongodb.net/?appName=Cluster0"
+).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+app.listen(3100, () => {
+  console.log('Server is running on port 3100');
 });
